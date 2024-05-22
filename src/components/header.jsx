8c62fa6932a9e4ffece16601594792e95374fcfe
kjdev99 <jamshidqayimov0399@gmail.com>
2024-05-23 01:27:45 +0500

    header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -19,21 +19,21 @@ const Header = ({ modal }) => {
         mousewheel={true}
         keyboard={true}
         modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-        className="mySwiper h-[495px]"
+        className="mySwiper h-[495px] max-md:h-[280px] rounded"
       >
-        <SwiperSlide className="bg-transparent h-[495px]">
+        <SwiperSlide className="bg-transparent h-[495px] max-md:h-[280px]">
           <img
             src="./hero1.jpg"
             alt="hero"
-            className="!h-[495px] !w-[1200px] mx-auto"
+            className="!h-[495px] max-md:h-[280px] !w-[1200px] mx-auto"
             // onClick={() => modal.setModal(!modal.modal)}
           />
         </SwiperSlide>
-        <SwiperSlide className="bg-transparent h-[495px]">
+        <SwiperSlide className="bg-transparent h-[495px] max-md:h-[280px]">
           <img
             src="./hero2.jpg"
             alt="hero"
-            className="!h-[495px] !w-[1200px] mx-auto"
+            className="!h-[495px] max-md:h-[280px] !w-[1200px] mx-auto"
             // onClick={() => modal.setModal(!modal.modal)}
           />
         </SwiperSlide>
